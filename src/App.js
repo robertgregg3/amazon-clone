edfb2861d3ae8de42f010cb509127030936f60b3
Robert Gregg <hi@robertgregg.co.uk>
2021-01-18 20:19:57 +0000

    Added Router and RRD

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React from "react";
 import "./css/App.css";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <h1>I am the checkout</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
