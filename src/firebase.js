8be9aa2cd2bb4e23e3b091385a6ed38e43d39271
Robert Gregg <hi@robertgregg.co.uk>
2021-01-18 13:56:12 +0000

    Initial commit - amazon clone setup

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDpMXR7KIsNxKEVNLbBmww-qk_FtMur2HY",
+  authDomain: "clone-robg.firebaseapp.com",
+  projectId: "clone-robg",
+  storageBucket: "clone-robg.appspot.com",
+  messagingSenderId: "934854555820",
+  appId: "1:934854555820:web:ae02725ff111a74edc4709",
+};
