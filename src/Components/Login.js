d0ba5e35797a6d5721abda15a6d9cae4df60b51c
Robert Gregg <hi@robertgregg.co.uk>
2021-01-19 22:28:52 +0000

    Connected Firebase authentication

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/Login.css";
 import AmazonLogo from "../images/amazon_logo.svg";
 import { Link } from "react-router-dom";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignIn = (e) => {
+    e.preventDefault();
+  };
+  const handleRegister = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="login">
       <Link to="/">
@@ -15,11 +25,22 @@ function Login() {
 
         <form>
           <label>E-mail</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <label>Password</label>
-          <input type="password" />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-          <button className="amazon-standard-button login__signInButton">
+          <button
+            type="submit"
+            onClick={handleSignIn}
+            className="amazon-standard-button login__signInButton">
             Sign in
           </button>
         </form>
@@ -27,7 +48,7 @@ function Login() {
           By signing in you agree to the Amazon clone by Robert Gregg conditions
           of Use and Sale. This is a demo site so no orders will be processed.{" "}
         </p>
-        <button className="login__registerAccount">
+        <button onClick={handleRegister} className="login__registerAccount">
           Create your Amazon Account
         </button>
       </div>
