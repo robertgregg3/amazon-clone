039fc949791601b05e891350db7f955c037f5a4f
Robert Gregg <hi@robertgregg.co.uk>
2021-01-21 15:05:58 +0000

    updated API link with live link from firebase

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/clone-robg/us-central1/api", // api cloud function url
+  baseURL: "https://us-central1-clone-robg.cloudfunctions.net/api", // api cloud function url
 });
+
+// old local host api if we need tomake changes http://localhost:5001/clone-robg/us-central1/api
 
 export default instance;
